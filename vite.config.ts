73740be3abc4ee2931f59b4ba9ba3f1b99b0c7f6
tbressel <tbressel.dev@gmail.com>
2024-10-04 00:28:52 +0200

    FIX: HTTP2 protocol configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
+// Import the defineConfig function and the viteStaticCopy plugin
 import { defineConfig } from 'vite';
+
+// Import the plugin to copy static files
 import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+// Import the path module to resolve the path of the images folder
 import { resolve } from 'path';
+
+// Interract with file system to read the certificate and key
+import fs from 'fs';
+
 
 export default defineConfig({
   plugins: [
@@ -20,11 +29,16 @@ export default defineConfig({
   resolve: {
     alias: [
       { find: '@assets', replacement: resolve(__dirname, "images") },
+      { find: '@assets', replacement: resolve(__dirname, "datas") },
     ]
   },
   server: {
     host: '192.168.1.79', 
     port: 5173, 
     open: true,
+    https: {
+      key: fs.readFileSync('./server.key'),
+      cert: fs.readFileSync('./server.crt'),
+    },
   }
 });
